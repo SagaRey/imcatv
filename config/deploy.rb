9999116dbdcf045d891651a7d767ccacf3e3e846
sagarey <saga.rey@outlook.com>
2015-03-17 20:15:31 +0800

    update image_url

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,6 +49,7 @@ task :setup => :environment do
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
 
   queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
   queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml and secrets.yml'."]
 end
 
